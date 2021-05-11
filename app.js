e55a3a82570363f4583331f7c7b5afc76f89d4b3
praneetha tirupathi <tpraneetha@users.noreply.github.com>
2021-05-11 10:34:07 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const postData = async (url = "", data = {}) => {
+  const response = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+
+  try {
+    const newData = await response.json();
+    console.log(newData);
+    return newData;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+postData("/addMovie", { movie: "matrix", score: 5 });
+
+postData("/add", { answer: 42 });
